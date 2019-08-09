5e4c9ee0a591a596350a032fc58daae130abb0c1
STEFANO LUIZ FERREIRA JUNIOR <stefano.lfjunior@senacsp.edu.br>
2019-08-08 22:10:06 -0300

    Recursos lista.js Arrey

---
+++ lista.js
@@ -0,0 +1,28 @@
+
+//Antigamente
+//var listaAntiga = Array();
+
+
+let lista = []; //Novo Array
+console.log(lista);
+
+// Adicionando Uma lista
+// array pode colocar string, numero, boleano
+
+lista.push("Carvão")
+lista.push("Sal Grosso")
+lista.push("Cerveja")
+lista.push("Farinha")
+lista.push(10);
+
+console.log(lista);
+
+//index Todo arrey começa com 0 (zero)
+
+console.log(lista[0]);
+
+// remover da lista
+console.log(lista.pop()); //pop sempre remove o ultimo item da lista
+console.log(lista);
+
+
