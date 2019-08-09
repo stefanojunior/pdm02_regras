1a6b53571c192eae6bc4851b1cfee3c268f7122e
STEFANO LUIZ FERREIRA JUNIOR <stefano.lfjunior@senacsp.edu.br>
2019-08-08 21:11:46 -0300

    teste ambiente dev

--- regras.js
+++ regras.js
@@ -1,6 +1,10 @@
-//exibir
+
+//ambiente de desenvolvimento
+
 let nome = "Stefano"; //variavel
 var nome1 = "Stefano"; //constante
 console.log ("Stefano")
+
+console.log(nome + " Este é o meu nome" + nome);
 
 
